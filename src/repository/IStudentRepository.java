52240432247d662e2c41bbae1a12ece87c4f5270
A <A@LAPTOP-FPQ2GO8P>
2023-10-17 16:52:04 +0700

    commit 16

---
+++ src/repository/IStudentRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+
+import java.util.ArrayList;
+import model.Student;
+
+public interface IStudentRepository {
+    
+    void addStudent(ArrayList<Student> students);
+    
+    void print(ArrayList<Student> students);
+    
+    void display();
+    
+    
+}
